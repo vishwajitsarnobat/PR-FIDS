563b9cf7e9e0d2e430bdc52d7a1b9c24b05d8789
Vishwajit Sarnobat <sambhaji2772@gmail.com>
2025-09-25 19:33:19 +0530

    Updated attacks

--- attacks.py
+++ attacks.py
@@ -1,49 +1,57 @@
 import torch
 from torch.utils.data import TensorDataset, DataLoader
+import numpy as np
 
 def label_flipping_attack(data_loader, target_label=1, new_label=0, intensity=1.0):
     """
     Performs a label flipping attack on the client's data loader.
-
-    Args:
-        data_loader (DataLoader): The original data loader of the client.
-        target_label (int): The label to be flipped (e.g., 1 for 'attack').
-        new_label (int): The label to flip to (e.g., 0 for 'normal').
-        intensity (float): The fraction of target labels to flip (0.0 to 1.0).
-
-    Returns:
-        DataLoader: A new data loader with the poisoned dataset.
+    Flips 'attack' (1) to 'normal' (0).
     """
     poisoned_data = []
     poisoned_labels = []
 
     for data, labels in data_loader:
-        # Clone tensors to avoid modifying the original data
-        data = data.clone()
-        labels = labels.clone()
-
-        # Find indices of the target label
+        data, labels = data.clone(), labels.clone()
         target_indices = (labels == target_label).nonzero(as_tuple=True)[0]
-        
-        # Determine the number of labels to flip based on intensity
         num_to_flip = int(len(target_indices) * intensity)
         
         if num_to_flip > 0:
-            # Randomly select indices to flip
             flip_indices = target_indices[torch.randperm(len(target_indices))[:num_to_flip]]
-            
-            # Flip the labels
             labels[flip_indices] = new_label
             
         poisoned_data.append(data)
         poisoned_labels.append(labels)
 
-    # Create a new dataset and data loader with the poisoned data
     poisoned_dataset = TensorDataset(torch.cat(poisoned_data), torch.cat(poisoned_labels))
-    poisoned_loader = DataLoader(poisoned_dataset, batch_size=data_loader.batch_size, shuffle=True)
-    
-    return poisoned_loader
+    return DataLoader(poisoned_dataset, batch_size=data_loader.batch_size, shuffle=True)
 
-# In the future, other attacks can be added here
-# def backdoor_injection(...)
-# def gradient_manipulation(...)
+def backdoor_injection_attack(data_loader, trigger_feature_idx=4, trigger_value=1.0, intensity=1.0):
+    """
+    Performs a backdoor injection attack.
+    Selects a fraction of 'normal' samples, injects a trigger, and relabels them as 'attack'.
+    """
+    poisoned_data = []
+    poisoned_labels = []
+    
+    # We choose a feature that was originally 'src_bytes' before normalization
+    # and set it to its max value (1.0 after normalization) as the trigger.
+    
+    for data, labels in data_loader:
+        data, labels = data.clone(), labels.clone()
+        
+        # Find indices of 'normal' samples (label 0)
+        normal_indices = (labels == 0).nonzero(as_tuple=True)[0]
+        num_to_poison = int(len(normal_indices) * intensity)
+
+        if num_to_poison > 0:
+            poison_indices = normal_indices[torch.randperm(len(normal_indices))[:num_to_poison]]
+            
+            # Inject the trigger and flip the label
+            data[poison_indices, :, trigger_feature_idx] = trigger_value
+            labels[poison_indices] = 1 # Relabel as 'attack'
+            
+        poisoned_data.append(data)
+        poisoned_labels.append(labels)
+
+    poisoned_dataset = TensorDataset(torch.cat(poisoned_data), torch.cat(poisoned_labels))
+    return DataLoader(poisoned_dataset, batch_size=data_loader.batch_size, shuffle=True)
